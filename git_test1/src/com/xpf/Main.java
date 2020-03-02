3264a383ccf11aeb25eee8a9e448f149193b559e
ban <415047736@qq.com>
2020-03-02 16:59:55 +0800

    测试

---
+++ git_test1/src/com/xpf/Main.java
@@ -0,0 +1,16 @@
+package com.xpf;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("156688");
+        System.out.println("156688");
+        System.out.println("156688");
+        System.out.println("156688");
+        System.out.println("156688");
+        System.out.println("156688");
+        System.out.println("156688");
+        System.out.println("156688");
+    }
+}
